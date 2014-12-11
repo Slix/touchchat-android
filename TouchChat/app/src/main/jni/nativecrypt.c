f3ca04e91605a905ff5d326233c71c8e98490b39
Peter Kowalczyk <slixpk@gmail.com>
2014-12-10 20:16:57 -0600

    Beginnings of key gen and config

--- TouchChat/app/src/main/jni/nativecrypt.c
+++ TouchChat/app/src/main/jni/nativecrypt.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <openssl/rsa.h>
+#include "openssl/rsa.h"
 
 #include "nativecrypt.h"
+
+const int BITS = 2048;
+const unsigned long EXPONENT = 65537;
 
 void Java_net_bytesec_touchchat_Native_helloworld(JNIEnv* env) {
 	printf("Hello, world!\n");
@@ -10,6 +13,12 @@ void Java_net_bytesec_touchchat_Native_helloworld(JNIEnv* env) {
 }
 
 jstring Java_net_bytesec_touchchat_Native_generateKeypair(JNIEnv* env) {
+    // Set up exponent
+    BIGNUM *e = BN_new();
+    BN_set_word(e, EXPONENT);
+
+    RSA *r = RSA_new();
+    RSA_generate_key_ex(r, BITS, e, NULL);
 }
 
 jstring Java_net_bytesec_touchchat_Native_signMessage(
